b14f191d26b8d415170c23706aba370ac0c18931
Ikwuka CySec <242934426+IkwukaCySec@users.noreply.github.com>
2025-12-29 19:13:59 -0500

    List full table and its values

---
+++ program_and_debug/0x02-sql_intro/5-full_table.sql
@@ -0,0 +1,2 @@
+-- Print the full description of the table named `defensive_security`.
+SHOW CREATE TABLE `defensive_security`;
---
+++ program_and_debug/0x02-sql_intro/6-list_values.sql
@@ -0,0 +1,2 @@
+-- List all rows of the table named `defensive security`.
+SELECT * FROM `defensive_security`;
