13cf52b8b434c7c5426a0b09389ae97f90bbf193
Ikwuka CySec <242934426+IkwukaCySec@users.noreply.github.com>
2025-12-29 19:14:31 -0500

    Insert value and count 7

---
+++ program_and_debug/0x02-sql_intro/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- Insert a new row in the `defensive_security` table.
+INSERT INTO `defensive_security` (`id`, `name`) VALUE (7, "Ikwuka CySec");
---
+++ program_and_debug/0x02-sql_intro/8-count_7.sql
@@ -0,0 +1,4 @@
+-- Display the number of records with id=7 in the `defensive_security` table.
+SELECT COUNT(*)
+FROM `defensive_security`
+WHERE `id` = 7;
