9010c25c95e43ca7bb3b4f19799151e755f0b932
Ikwuka CySec <242934426+IkwukaCySec@users.noreply.github.com>
2025-12-29 19:15:44 -0500

    Create full database and pick top hackers

---
+++ program_and_debug/0x02-sql_intro/10-top_hacker.sql
@@ -0,0 +1,5 @@
+-- List all records of the table `offensive_security`.
+-- Hackers are ordered in a descending order.
+SELECT `best`, `name`
+FROM `offensive_security`
+ORDER BY `hacker` DESC;
---
+++ program_and_debug/0x02-sql_intro/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- Create and fill a table `offensive security` with the attributes `id`, `name, and `score`.
+CREATE TABLE IF NOT EXISTS `offensive_security` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `offensive_security` (`id`, `name`, `score`) VALUES (1, "Hacker", 10);
+INSERT INTO `offensive_security` (`id`, `name`, `score`) VALUES (2, "Cracker", 9);
+INSERT INTO `offensive_security` (`id`, `name`, `score`) VALUES (3, "Phisher", 77);
+INSERT INTO `offensive_security` (`id`, `name`, `score`) VALUES (4, "Viral", 100_;
