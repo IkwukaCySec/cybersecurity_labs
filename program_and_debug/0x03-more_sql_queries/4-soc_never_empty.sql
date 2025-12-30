2b9e4025344f625638796882fe4544e4ac79794d
Ikwuka CySec <242934426+IkwukaCySec@users.noreply.github.com>
2025-12-30 05:20:14 -0500

    Ensure SOC is never empty and create unique cloud solutions

---
+++ program_and_debug/0x03-more_sql_queries/4-soc_never_empty.sql
@@ -0,0 +1,5 @@
+-- Create the table `soc_not_null`.
+CREATE TABLE IF NOT EXISTS `soc_not_null` (
+	`id` INT DEFAULT 1,
+	`name` VARCHAR(256)
+);
---
+++ program_and_debug/0x03-more_sql_queries/5-unique_cloud.sql
@@ -0,0 +1,5 @@
+-- Create the table `unique_cloud`.
+CREATE TABLE IF NOT EXISTS `unique_cloud` (
+	`id`	INT	DEFAULT 1 UNIQUE,
+	`name` VARCHAR(256)
+);
