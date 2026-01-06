8ece79fe4f06c7121210f71bc45af0c1350b06c9
Ikwuka CySec <242934426+IkwukaCySec@users.noreply.github.com>
2026-01-05 20:29:25 -0500

    Create AWS defenses

---
+++ tryhackme/advent_of_cyber/0x23-aws_security/defenses/s3_hardening.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "secure_bucket" {
+  bucket = "toy-factory-secure"
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.secure_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
