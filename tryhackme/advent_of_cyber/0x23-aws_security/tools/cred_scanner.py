19ecedb74b1076699bbdae7244516919447017fa
Ikwuka CySec <242934426+IkwukaCySec@users.noreply.github.com>
2026-01-05 20:38:54 -0500

    Scan AWS credentials

---
+++ tryhackme/advent_of_cyber/0x23-aws_security/tools/cred_scanner.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Scan S3 buckets for credentials
+
+import boto3
+import re
+
+s3 = boto3.client('s3', endpoint_url='http://localhost:4566')
+
+buckets = s3.list_buckets()['Buckets']
+
+cred_pattern = re.compile(r'AKIA[0-9A-Z]{16}')
+
+for bucket in buckets:
+    name = bucket['Name']
+    try:
+        objs = s3.list_objects_v2(Bucket=name)
+        for obj in objs.get('Contents', []):
+            key = obj['Key']
+            data = s3.get_object(Bucket=name, Key=key)
+            content = data['Body'].read().decode()
+            if cred_pattern.search(content):
+                print(f"[!] CREDENTIALS FOUND in {name}/{key}")
+    except:
+        pass
